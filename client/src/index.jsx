db84edf231d7bb244e43b7c3857dbebd3bbc3669
Shasikumar10 <shashikumar44887@gmail.com>
2025-10-22 23:10:33 +0530

    All updated folders

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/App.css';
+import './styles/App.css'; // adjust path if your global CSS file differs
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container not found. Make sure index.html has <div id="root"></div>');
+
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
